8aaddd448cada87e1cac647bb92e9d0825c49bb7
eden <eden.quan.chen@gmail.com>
2024-11-05 01:05:27 +0800

    biz-kit - rsa tools use configuration instead of file / add more utility's function

--- encoding/encrypt/rsa.go
+++ encoding/encrypt/rsa.go
@@ -9,21 +9,23 @@ import (
 	"errors"
 	"github.com/go-kratos/kratos/v2/log"
 	"golang.org/x/crypto/bcrypt"
-	"os"
 )
 
 type RsaTool struct {
-	log            *log.Helper
-	PrivateKeyFile string
+	log        *log.Helper
+	PrivateKey string
+	PublicKey  string
 }
 
-func NewRsaTool(logger log.Logger, privateKeyFile string) *RsaTool {
+func NewRsaTool(logger log.Logger, publicKey string, privateKey string) *RsaTool {
 	return &RsaTool{
-		log:            log.NewHelper(logger),
-		PrivateKeyFile: privateKeyFile,
+		log:        log.NewHelper(logger),
+		PrivateKey: privateKey,
+		PublicKey:  publicKey,
 	}
 }
 
+// ValidateHash 校验 psw 是否与已经过 Hash 的密码 hashed 相等
 func (r *RsaTool) ValidateHash(psw string, hashed string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(psw))
 	return err == nil
@@ -42,7 +44,9 @@ func (r *RsaTool) GenerateHash(pwd string) (string, error) {
 // 具体步骤为：
 //
 // 1. 对传递的密码进行 Base64 Decode;
+//
 // 2. 通过私钥进行解密得到密码明文
+//
 // 3，通过 BCrypt 生成密码的 Hash (一般服务器只存储 Hash)
 func (r *RsaTool) GeneratePassword(password string) (string, string, error) {
 	originPwd, err := r.DecryptPassword(password)
@@ -58,11 +62,26 @@ func (r *RsaTool) GeneratePassword(password string) (string, string, error) {
 	return originPwd, pwd, nil
 }
 
+// DecryptAndValidate 将对 pswNeedDecrypt 进行解密得到明文后，与已经生成的 Hash hashedPsw 进行校验，
+// 如果两者相等这返回 nil, 否则返回对应的错误信息
+func (r *RsaTool) DecryptAndValidate(pswNeedDecrypt, hashedPsw string) error {
+	decryptPwd, err := r.DecryptPassword(pswNeedDecrypt)
+	if err != nil {
+		return err
+	}
+
+	if r.ValidateHash(decryptPwd, hashedPsw) {
+		return nil
+	}
+
+	return errors.New("password error")
+}
+
 // DecryptPassword 对密码进行解密，传递的密码已经经过了 Base64 Encode
 func (r *RsaTool) DecryptPassword(password string) (string, error) {
 	// 解密密码
 	decoded, err := base64.StdEncoding.DecodeString(password)
-	passwordOutput, err := r.DecryptRSA(decoded, r.PrivateKeyFile)
+	passwordOutput, err := r.DecryptRSA(decoded, r.PrivateKey)
 	passwordOutputStr := string(passwordOutput)
 
 	if len(passwordOutputStr) < 8 {
@@ -75,47 +94,22 @@ func (r *RsaTool) DecryptPassword(password string) (string, error) {
 
 // EncryptPassword 对密码进行解密
 func (r *RsaTool) EncryptPassword(password string) (string, error) {
-	pwdEncrypted, err := r.EncryptRSA([]byte(password), r.PrivateKeyFile)
+	pwdEncrypted, err := r.EncryptRSA([]byte(password), r.PrivateKey)
 	return string(pwdEncrypted), err
 }
 
 // DecryptRSA 对数据进行解密操作
-func (r *RsaTool) DecryptRSA(src []byte, path string) (res []byte, err error) {
+func (r *RsaTool) DecryptRSA(src []byte, privKey string) (res []byte, err error) {
 	//1.获取秘钥（从本地磁盘读取）
-	f, err := os.Open(path)
-	if err != nil {
-		return
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	fileInfo, _ := f.Stat()
-	b := make([]byte, fileInfo.Size())
-	_, _ = f.Read(b)
-	block, _ := pem.Decode(b)                                 //解码
+	block, _ := pem.Decode([]byte(privKey))                   //解码
 	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes) //还原数据
 	res, err = rsa.DecryptPKCS1v15(rand.Reader, privateKey, src)
 	return
 }
 
-func (r *RsaTool) EncryptRSA(src []byte, path string) (res []byte, err error) {
-	//1.获取秘钥（从本地磁盘读取）
-	f, err := os.Open(path)
-	if err != nil {
-		return
-	}
-
-	defer func() {
-		_ = f.Close()
-	}()
-
-	fileInfo, _ := f.Stat()
-	b := make([]byte, fileInfo.Size())
-	_, _ = f.Read(b)
+func (r *RsaTool) EncryptRSA(src []byte, pubKey string) (res []byte, err error) {
 	// 2、将得到的字符串解码
-	block, _ := pem.Decode(b)
+	block, _ := pem.Decode([]byte(pubKey))
 
 	// 使用X509将解码之后的数据 解析出来
 	//x509.MarshalPKCS1PublicKey(block):解析之后无法用，所以采用以下方法：ParsePKIXPublicKey
@@ -125,7 +119,7 @@ func (r *RsaTool) EncryptRSA(src []byte, path string) (res []byte, err error) {
 		return
 	}
 	//4.使用公钥加密数据
-	pubKey := keyInit.(*rsa.PublicKey)
-	res, err = rsa.EncryptPKCS1v15(rand.Reader, pubKey, src)
+	pKey := keyInit.(*rsa.PublicKey)
+	res, err = rsa.EncryptPKCS1v15(rand.Reader, pKey, src)
 	return
 }
