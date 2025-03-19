7042a8692e33bdc79656d0b23b88bd4bab7d6321
SinChenPure <sin.chen@pureglobal.cn>
2025-03-19 13:52:21 +0800

    optimize - fix bugs of parallel usd with snowfake case

--- encoding/snowfake/snowfake.go
+++ encoding/snowfake/snowfake.go
@@ -2,11 +2,19 @@ package snowfake
 
 import (
 	"github.com/bwmarrin/snowflake"
+	"sync"
+	"time"
 )
 
+var snow *snowflake.Node
+var once sync.Once
+
+// NewSnowflakeID 使用当前时间戳作为 Seed 创建 Snowflake 的实例
 func NewSnowflakeID() int64 {
-	node, _ := snowflake.NewNode(1)
+	once.Do(func() {
+		snow, _ = snowflake.NewNode(time.Now().UnixNano())
+	})
 	// Generate a snowflake ID.
-	id := node.Generate().Int64()
+	id := snow.Generate().Int64()
 	return id
 }
